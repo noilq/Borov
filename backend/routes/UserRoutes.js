297b6db69aace54dbc61328701db031569849b79
noilq <tutovartem2024n@gmail.com>
2024-04-24 21:56:34 +0200

    Backend validation chain for user creation

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,7 +2,22 @@ const express = require('express')
 const router = express.Router()
 const db = require('../database')
 const crypto = require('crypto');
+const { body, validationResult } = require('express-validator');
 
+//const body('password_hash') = body('password_hash').trim().slice(0, 256); 
+const createUserValidationChain = [
+    body('login')
+        .isLength({ min: 4, max: 32}).withMessage('Login must be between 4-32 characters.')
+        .matches(/^[a-zA-Z0-9]+$/).withMessage('Login must contain only letters and numbers.'),
+    body('password_hash')
+        .isLength({ min: 8}).withMessage('Password must be at least 8 characters long.')
+        .custom(value => !/\s/.test(value)).withMessage('Password must not contain spaces.'),
+    body('name')
+        .isLength({ min: 4, max: 50}).withMessage('Name must be between 4-50 characters.'),
+    body('description')
+        .optional()
+        .isLength({ max: 500}).withMessage('Description must be at most 500 characters long.')
+]
 
 router.get('/', (req, res) => {
     const sql = 'SELECT * FROM users'
@@ -13,7 +28,13 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/create', (req, res) => {
+router.post('/create', createUserValidationChain, (req, res) => {
+    const validationErrors = validationResult(req)
+
+    if (!validationErrors.isEmpty()) {
+        return res.status(400).json({ errors: validationErrors.array() });
+    }
+
     const userData = req.body
 
     userData.password_hash = crypto.createHash('md5').update(userData.password_hash).digest('hex');
