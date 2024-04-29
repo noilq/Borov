6b2b7909e33c3d3efe667a14392d0d1737ba1b1d
noilq <tutovartem2024n@gmail.com>
2024-04-29 23:48:50 +0200

    Post edit method

--- backend/routes/PostRoutes.js
+++ backend/routes/PostRoutes.js
@@ -13,11 +13,15 @@ const postValidationChain = () => [
 ]
 
 router.get('/', verifyToken, (req, res) => {
-    const sql = 'SELECT * FROM posts'
-    db.query(sql, (err, data) => {
+    const postId = req.query.id
+
+    const sql = 'SELECT * FROM posts WHERE id = ?'
+    db.query(sql, [postId], (err, result) => {
         if (err) 
-            return res.json(err)
-        res.json(data)
+            return res.json(err);
+        if (result.length === 0)
+            return res.status(404).json({ error: 'Post not found.' });
+        res.json(result[0])
     })
 })
 
@@ -41,8 +45,44 @@ router.post('/create', verifyToken, postValidationChain(), (req, res) => {
     })
 })
 
+router.post('/edit', verifyToken, postValidationChain(), (req, res) => {
+    const validationErrors = validationResult(req)
+
+    if(!validationErrors.isEmpty())
+        return res.status(400).json( { errors: validationErrors.array() })
+
+    const postId = req.query.id  
+    
+    const user = req.user
+    let sql = 'SELECT * FROM posts WHERE id = ?'
+
+    db.query(sql, [postId], (err, result) => {
+        if(err)
+            return res.json(err)
+
+        if(result.length === 0)
+            return res.status(404).json({ error: 'Post not found.'})
+
+        post = result[0]
+        
+        if(!post == user.userId)
+            return res.status(404).json({ error: 'Now allowed.'})   
+    })
+    
+    const newPostData = req.body
+
+    sql = 'UPDATE posts SET title = ?, content = ?, category_id = ? WHERE id = ?'
+
+    db.query(sql, [newPostData.title, newPostData.content, newPostData.category_id, postId], (err, result) => {
+        if(err)
+            return res.json(err)
+
+        return res.json(result)
+    })
+})
+
 router.post('/delete', verifyToken, (req, res) => {
-    const postId = req.body.postId
+    const postId = req.query.id
     
     const user = req.user
     let sql = 'SELECT * FROM posts WHERE id = ?'
@@ -59,7 +99,7 @@ router.post('/delete', verifyToken, (req, res) => {
         if(!post == user.userId)
             return res.status(404).json({ error: 'Now allowed.'})   
     })
-    
+
     sql = 'DELETE FROM posts WHERE id = ?;'
 
     db.query(sql, [postId], (err, result) => {
