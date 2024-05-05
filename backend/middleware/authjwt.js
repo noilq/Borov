4d1779212bd2889445a652ddc272e30094bc07b7
noilq <tutovartem2024n@gmail.com>
2024-05-05 19:18:11 +0200

    Fixed auth

--- backend/middleware/authjwt.js
+++ backend/middleware/authjwt.js
@@ -28,7 +28,7 @@ function verifyToken(req, res, next) {
                 const newAccessToken = generateToken(payload.userId, payload.login, 1800)
     
                 res.cookie('refreshToken', refreshToken).header('Authorization', newAccessToken).send({ user: req.user })
-                return
+                
             }catch(err){
                 return res.status(401).json({ error: 'Invalid token.'})
             }
